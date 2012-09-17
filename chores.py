151380d140d4113aeb6192a857d6f3d9a5d383e3
Eli Daniel <eli@gahooa.com>
2012-09-17 18:46:52 -0400

    Added names instead of numbers for days and some other changes

--- chores.py
+++ chores.py
@@ -4,6 +4,36 @@
 import time
 import sys
 
+class Day:
+  Mon = 0
+  Tue = 1
+  Wed = 2
+  Thu = 3
+  Fri = 4
+  Sat = 5
+  Sun = 7
+  Names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  
+
+People = ['Eli', 'Ezra', 'Zech']
+
+#------------------------------------------------------------------------------
+
+ChoreList = []
+
+def AddChore(PersonList, Chore):
+  print "Adding {0} for {1}".format(Chore, str(ChoreList))
+  
+  for person in PersonList:
+    if person in People:
+       break
+  else:
+    person = 'NONE'
+    
+  ChoreList.append((person, Chore))
+
+#------------------------------------------------------------------------------
+
 # Get today to use as the default date input
 today = time.strftime('%Y-%m-%d')
 
@@ -36,7 +66,7 @@ dayofweek = workdate.tm_wday
 
 print
 print "The day of the year is:", dayofyear
-print "The day of the week (monday is 0):", dayofweek
+print "The day of the week is:", Day.Names[dayofweek]
 
 mod3 = dayofyear % 3
 
@@ -47,24 +77,28 @@ print
 # --------------------------------------------------------------------------------
 # This is the cat chore
 
+print "----- Kitty Cat Chore -----"
+
 print 'Ezra: do the water chore'
 print 'Ezra: feed and water the cat'
 
-if dayofweek in (3,6):
+if dayofweek in (Day.Thu, Day.Sun):
   print 'Ezra: dump litter; clear litter; add new litter; clean around litter'
 else:
   print 'Ezra: scoop the litter; sweep if needed, double check!'
 
+print
 
 # --------------------------------------------------------------------------------
 # This is the sweeping chore
 
-if dayofweek != 6:
+if dayofweek != Day.Sun: 
   print 'Ezra: sweep the living room and dining room'
   print 'Ezra: scrub the living room and dining room as needed'
   print 'Eli : sweep the kitchen'
-  print' Eli: do the bathroom'
   print 'Eli : scrub the kitchen'
+  print 'Zech: clean the  living room and dining room; but only if needed'
+  print 'Zech: do the trash chore'
 else:   
   print 'Ezra: sweep the living room and dining room, but only if needed'
   print 'Ezra: scrub the living room and dining room, but only if needed'
@@ -72,9 +106,25 @@ else:
   print 'Eli : scrub the kitchen, but only if needed'
 
 # --------------------------------------------------------------------------------
+# This is the Bathroom chore
+
+print "----- Bathroom Chore -----"
+
+if dayofweek != Day.Sun: 
+  print 'Eli : do the bathroom'
+else:   
+  print 'Eli : clean the bathroom sink and toliet, but only if needed'
+   
+print 
+
+# --------------------------------------------------------------------------------
 # This is the dishes chore
 
+print '----- Dishes Chore -----'
+
 print 'Eli : Do the dishes; do them again as needed all day'
+
+print
 
 # --------------------------------------------------------------------------------
 # This is the Laundry Chore
